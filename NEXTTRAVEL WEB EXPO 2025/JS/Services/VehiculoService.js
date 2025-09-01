ee653adfd81fdea7c2dea6dea00b6330740eaf91
Zelayita <20240181@ricaldone.edu.sv>
2025-08-31 21:00:13 -0600

    ya sirve el Get

--- NEXTTRAVEL WEB EXPO 2025/JS/Services/VehiculoService.js
+++ NEXTTRAVEL WEB EXPO 2025/JS/Services/VehiculoService.js
@@ -2,85 +2,91 @@
 
 //Asi es como se mostraran los datos en la API
 /*
-  "idVehiculo": 1,
-      "placa": "P123XYZ",
-      "marca": "Toyota",
-      "modelo": "Corolla",
-      "anio": 2020,
-      "fechaVencimientoCirculacion": "2025-12-31",
-      "fechaVencimientoSeguro": "2025-11-30",
-      "fechaVencimientoRevision": "2025-10-31"
+    "idVehiculo": 4,
+    "placa": "M-987654",
+    "modelo": "Honda CB125F",
+    "capacidad": 1,
+    "anioFabricacion": 2021,
+    "estado": "Activo"
 */ 
 
-//const API_URL = 'http://localhost:8080/apivehiculo';
+const API_URL = 'http://localhost:8080/api/vehiculos';
 
 
-/*export async function ObtenerVehiculos(page = 0, size = 10){
-    const res = await fetch(`${API_URL}/vehiculos?page=${page}&size=${size}`)
-    return res.json();   
+export async function ObtenerVehiculos(page = 0, size = 10){
+    try{
+        const res = await fetch(`${API_URL}/vehiculos/listar?page=${page}&size=${size}`) 
+        if(!res.ok)throw new Error(`Error al Buscar Vehiculos: ${res.status}`);
+        return res.json();  
+    }catch(error){
+        console.error("Error el Buscar vehiculos:", error);
+        throw error;
+    } 
 }
 
-export async function ObtenerVehiculosPorID(page = 0, size = 10, id){
-    const res = await fetch(`${API_URL}/vehiculos/vehiculos/${id}?page=${page}&size=${size}`)
-    return res.json();   
+export async function ObtenerVehiculosPorID(page = 0, size = 10, modelo){
+    try{
+        const res = await fetch(`${API_URL}/vehiculos/buscar/modelo/${modelo}?page=${page}&size=${size}`);
+        if(!res.ok)throw new Error(`Error al Buscar vehiculos por Modelo: ${res.status}`); 
+        return res.json();
+    }catch(err){
+        console.error("Error el buscar", err);
+        throw err;
+    }  
 }
-
-
 
 export async function InsertarVehiculos(data){
-    const res = await fetch(`${API_URL}/insertar`, {
+    try{
+        const res = await fetch(`${API_URL}/vehiculos`, {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify(data),
     });
+    if(!res.ok)throw new Error(`Error al Insertar Vehiculos: ${res.status}`);
+    }catch(er){
+        console.error(`Error al insertar Vehiculo:`, er);
+        throw er;
+    }
 }
 
 export async function ActualizarVehiculo(id, data){
-    await fetch(`${API_URL}/actualizar/${id}`,{
+    try{
+        const res = await fetch(`${API_URL}/actualizar/${id}`,{
         method: 'PUT',
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify(data),
     });
+    if (!res.ok) throw new Error(`Error al Actualizar el vehiculo: ${res.status}`);
+    return await res.json();
+    }catch(err){
+        console.error("Error al Actualizar Vehiculo", err);
+        throw err;
+    } 
 }
 
 export async function BorrarVehiculo(id){
-    await fetch(`${API_URL}/eliminar/${id}`, {method: 'DELETE'});
-}*/
-
-
-
-const API_URL = 'https://retoolapi.dev/Iz9uar/NextTarvel'
-
-
-
-export async function ObtenerVehiculos(page = 0,size = 10){
-    const res = await fetch(`${API_URL}/Iz9uar/NextTarvel?_page=${page}&_limit=${size}`)
-    return res.json();   
-};
-
-
-export async function InsertarVehiculos(data){
-    const res = await fetch(`${API_URL}/Iz9uar/NextTarvel`, {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify(data),
-    });
+    try{
+        const res = await fetch(`${API_URL}/eliminar/${id}`, {method: 'DELETE'});
+        if(!res.ok)throw new Error(`Error a la hora de Borrar vehiculo: ${res.status}`)
+    }catch(err){
+        console.error(`Error en la Hora de Eliminar: ${err}`);
+        throw err;
+    }
 }
 
-//Iz9uar/NextTarvel/1
-
-export async function ActualizarVehiculo(id, data){
-    await fetch(`${API_URL}/Iz9uar/NextTarvel/${id}`,{
-        method: 'PUT',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify(data),
-    });
+// /vehiculos/buscar/placa/
+//Buscar por Placa
+export async function BuscarPorPlaca(id, page = 0, size = 5){
+   try{
+        const res = await fetch(`${API_URL}/vehiculos/buscar/placa/${id}?page=${page}&size=${size}`);
+        if (!res.ok) throw new Error(`Error en la petición: ${res.status}`);
+        return await res.json();
+   }catch(error){
+        console.error(`Error en la hora de Obtener Vehiculos Por Placa:${error}`);
+        throw error;
+   }  
 }
 
-
-export async function BorrarVehiculo(id){
-    await fetch(`${API_URL}/Iz9uar/NextTarvel/${id}`, {method: 'DELETE'});
-}
 
 
 
